cdea34434d85de3af2634c9ba5f1ac593c61a439
Ali Haydar Oezdag <alihaydarozdag@gmail.com>
2024-06-09 13:18:38 +0200

    new web sites added

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
-chrome.storage.sync.get(['dersCalisMetni'], function(result) {
-    const metin = result.dersCalisMetni || 'DERS CALIS';
+chrome.storage.sync.get(['girilen_metin'], function(result) {
+    const metin = result.girilen_metin || 'DERS CALIS';
     document.body.innerHTML = `<h1 style="color:red; text-align:center; margin-top:20%;">${metin}</h1>`;
   });
   
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 document.getElementById('kaydet').addEventListener('click', function() {
-    var metin = document.getElementById('dersCalisMetni').value;
-    chrome.storage.sync.set({'dersCalisMetni': metin}, function() {
+    var metin = document.getElementById('girilen_metin').value;
+    chrome.storage.sync.set({'girilen_metin': metin}, function() {
         alert('Metin kaydedildi!');
     });
 });
